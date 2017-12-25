b7c97aa2794edd67273879fadb7cb02f51887926
Yan <yanshney@bramdeis.edu>
2017-12-24 23:57:18 -0500

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171224051946) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "pages", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
